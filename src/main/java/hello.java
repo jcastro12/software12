c6d3cf4d2eca91c19ebb9187b519c702413b955f
Joshua C <113030588+jcastro12@users.noreply.github.com>
2025-04-15 23:36:17 -0400

    Add files via upload

---
+++ src/main/java/hello.java
@@ -0,0 +1,15 @@
+/**
+ * Class for the Hello World application.
+ */
+public class hello
+{
+    /**
+     * Main method that prints Hello WPI.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello WPI!");
+    }
+}
